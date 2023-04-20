058804225b5292148549f844e6cf8871051a5ead
k-zyw <jianwuliyuan448@gmail.com>
2023-04-20 14:57:36 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
-  validates :name,                 presence: true
-  validates :profile,              presence: true
-  validates :occupation,           presence: true  
-  validates :position,             presence: true
-  validates :reset_password_token, presence: true
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         validates :name,                 presence: true
+         validates :profile,              presence: true
+         validates :occupation,           presence: true  
+         validates :position,             presence: true
+  
          has_many :prototypes, dependent: :destroy
          has_many :comments,   dependent: :destroy # commentsテーブルとのアソシエーション       
 end
